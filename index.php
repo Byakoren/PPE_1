01ed076eeb6834998822c05f2394cef102ab963d
Mehdi <naouimehdi@hotmail.fr>
2025-03-20 16:46:15 +0100

    Refactor index.php to include head.php and update styles for better layout management

--- index.php
+++ index.php
@@ -3,18 +3,9 @@
 <!--Mathieu MOURAO,Thomas GOUEZ et Mehdi NAOUI-->
 <DOCTYPE html>
 <html lang=en>
-    <head>
-        
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link  href="styles.css" rel="stylesheet">
-        <title>Emargement Gefor</title>
-        <script src="https://kit.fontawesome.com/b90a9ecdfa.js" crossorigin="anonymous"></script>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js"></script>
-    </head>
+    <?php
+        require "head.php";
+    ?>
  <body>
     <header>
         <?php
