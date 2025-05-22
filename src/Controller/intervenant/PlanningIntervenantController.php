883f4db8e74c2f07e89b8dc9e3adc161b6e5d74d
Mehdi <naouimehdi@hotmail.fr>
2025-05-22 14:52:34 +0200

    Refactor PlanningIntervenantController and update index template structure

--- src/Controller/intervenant/PlanningIntervenantController.php
+++ src/Controller/intervenant/PlanningIntervenantController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller\intervenant;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use App\Controller\intervenant\PlanningIntervenantController;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class PlanningIntervenantController extends AbstractController
 {
     #[Route('/planning/intervenant', name: 'app_planning_intervenant')]
     public function index(): Response
     {
+        
+        
         return $this->render('intervenant/planning_intervenant/index.html.twig', [
             'controller_name' => 'PlanningIntervenantController',
         ]);
