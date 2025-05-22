3cdd4451a112b995e0ad1165bfd3eb0f36f566ef
Byakoren <thomas.gouez-hoarau@live.fr>
2025-05-22 15:07:00 +0200

    Modification des pages planning login et emargement avec création des controllers pour connection à la BDD

---
+++ src/Controller/PlanningController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CoursRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class PlanningController extends AbstractController
+{
+    #[Route('/planning', name: 'app_planning')]
+    public function index(CoursRepository $coursRepository): Response
+    {
+        $coursArray = array_map(function($cours) {
+            $creneau = $cours->getCrenaux();
+
+            // Combine date + heure début / fin
+            $start = new \DateTime($creneau->getDate()->format('Y-m-d') . ' ' . $creneau->getHeureDebut()->format('H:i:s'));
+            $end = new \DateTime($creneau->getDate()->format('Y-m-d') . ' ' . $creneau->getHeureFin()->format('H:i:s'));
+
+            return [
+                'id' => $cours->getId(),
+                'title' => $cours->getMatiere()->getType(),
+                'start' => $start->format('Y-m-d\TH:i:s'),
+                'end' => $end->format('Y-m-d\TH:i:s'),
+            ];
+        }, $coursRepository->findAll());
+
+        return $this->render('auth/planning.html.twig', [
+            'cours' => $coursArray
+        ]);
+    }
+}
