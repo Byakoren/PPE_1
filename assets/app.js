b4602e0b9cb4533754785d10dd65fed47275397d
Byakoren <thomas.gouez-hoarau@live.fr>
2025-05-25 17:39:41 +0200

    modification fullcalendar avec import

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,34 @@
-import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css';
+import './styles/fullcalendar/daygrid.css';
+import './styles/fullcalendar/timegrid.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// Import JS FullCalendar
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import frLocale from '@fullcalendar/core/locales/fr';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const calendarEl = document.getElementById('calendar');
+    if (!calendarEl || !window.calendarEvents) return;
+
+    const calendar = new Calendar(calendarEl, {
+        plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+        initialView: 'dayGridMonth',
+        locale: frLocale,
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        slotMinTime: "08:00:00",
+        slotMaxTime: "19:00:01",
+        events: window.calendarEvents,
+        eventClick: function (info) {
+            window.location.href = "/emargement/" + info.event.id;
+        }
+    });
+
+    calendar.render();
+});
