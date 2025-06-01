1b5240c99bec86a1c3007917c8a14c2a46ca6eba
Mehdi <naouimehdi@hotmail.fr>
2025-06-01 15:13:08 +0200

    fix : clarifier le comportement de la méthode de déconnexion avec le message d’exception

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -50,6 +50,7 @@ class AuthController extends AbstractController
     #[Route('/logout', name: 'app_logout')]
     public function logout(): void
     {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
 
     }
 }
