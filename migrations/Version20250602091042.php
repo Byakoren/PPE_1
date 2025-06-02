e65f83c86fee0999d62c09d05b14b427e26a494a
Mehdi <naouimehdi@hotmail.fr>
2025-06-02 13:37:42 +0200

    feat : ajouter des fonctionnalités de validation à l’entité Participer et implémenter la fonctionnalité de signature dans le modèle d’emargement intervenant

--- migrations/Version20250602091042.php
+++ migrations/Version20250602091042.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250530175352 extends AbstractMigration
+final class Version20250602091042 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20250530175352 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD reset_token VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE participer ADD validation TINYINT(1) NOT NULL, ADD date_validation DATETIME DEFAULT NULL, ADD retard SMALLINT NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP reset_token');
+        $this->addSql('ALTER TABLE participer DROP validation, DROP date_validation, DROP retard');
     }
 }
---
+++ src/Controller/intervenant/ValidationEmargementController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ValidationEmargementController extends AbstractController{
+    #[Route('/intervenant/validation/emargement', name: 'app_intervenant_validation_emargement')]
+    public function index(): Response
+    {
+        return $this->render('intervenant/validation_emargement/index.html.twig', [
+            'controller_name' => 'Intervenant/ValidationEmargementController',
+        ]);
+    }
+}
